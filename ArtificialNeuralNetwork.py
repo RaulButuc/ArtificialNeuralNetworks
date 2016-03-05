9b70ff7a30dce0cb609140297eed28d0c6f7faa2
Raul Butuc <raulbutuc@gmail.com>
2016-03-05 02:57:26 +0000

    Added util methods and finished Layer class

--- ArtificialNeuralNetwork.py
+++ ArtificialNeuralNetwork.py
@@ -1,3 +1,8 @@
+from ArtificialNeuralNetworkUtility import between
+from ArtificialNeuralNetworkUtility import make_matrix
+
+use_bias = 1
+
 class ArtificialNeuralNetwork:
 
 	def __init__(self):
@@ -14,5 +19,20 @@ class ArtificialNeuralNetwork:
 		
 class Layer:
 	
-	def __init__(self):
-		pass
+	def __init__(self, id, layer_size, prev_layer_size):
+		self.id = id
+		self.n_neurons = layer_size
+		self.bias_val = 1
+		
+		self.input = [0] * self.n_neurons
+		
+		self.output = [0] * (self.n_neurons + 1)
+		self.output[0] = self.bias_val
+		
+		self.error = [0] * self.n_neurons
+		
+		self.weight = make_matrix(prev_layer_size + use_bias, self.n_neurons)
+		for i in range(len(self.weight)):
+			for j in range(len(self.weight[i])):
+				self.weight[i][j] = between(-0.2, 0.2)
+		
